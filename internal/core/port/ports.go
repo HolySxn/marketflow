40fb725d121a681b6f68071fed7c099507b0064f
ashpring <artshpringer2006@gmail.com>
2025-07-14 16:23:42 +0500

    feat: add ports interfaces

---
+++ internal/core/port/ports.go
@@ -0,0 +1,25 @@
+package port
+
+import (
+	"context"
+	"marketflow/internal/core/domain"
+	"time"
+)
+
+type Cache interface {
+	AddPrice(ctx context.Context, data domain.MarketData) error
+	GetPricesByPeriod(ctx context.Context, exchange string, pair string, period time.Duration) ([]float64, error)
+	GetLatestPrice(ctx context.Context, exchange string, pair string) (domain.MarketData, error)
+}
+
+type RepositoryPort interface {
+	SaveAggregate(ctx context.Context, data domain.AggregatedData) error
+	GetAggregatesByPeriod(ctx context.Context, exchange string, pair string, period time.Duration) ([]domain.AggregatedData, error)
+	GetLatestAggregate(ctx context.Context, exchange string, pair string) (domain.AggregatedData, error)
+}
+
+type ExchangePort interface {
+	Start() <-chan domain.MarketData
+	Stop()
+	IsConnected() bool
+}
